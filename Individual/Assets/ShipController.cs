8bef3f9f7c2979bdfb2c8d550260abb9f7767a8e
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2020-05-02 21:12:55 -0400

    created project

---
+++ Individual/Assets/ShipController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    Rigidbody bod;
+    Vector3 euler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bod = GetComponent<Rigidbody>();
+        euler = new Vector3(0, 0, 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        Quaternion rotation = Quaternion.Euler(euler * h);
+        Debug.Log("h: " + h);
+        bod.MoveRotation(bod.rotation * rotation);
+
+    }
+}
